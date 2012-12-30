cfa349cf464a595a809106d8606158489f77e812
erocs <github@erocs.org>
2012-12-29 20:02:00 -0800

    Make it useable Add Gadget jar compiled against 1.4.6

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 Gadget
 ======
 A Minecraft Bukkit server plugin that allows inspection of all events that fire.
+
+Config file settings:
+- show_only_player_events: Boolean, Filters all events except those which were Player initiated
+- only_show_events: String List, If defined, provides the list of events to describe, is modified by hide_events and show_only_player_events
+- hide_events: String List, Excludes describing these events
+- hide_methods: String List, Excludes these method names while describing the event object
+
+Default configuration:
+- show_only_player_events: true
+- only_show_events:
+- hide_events: PlayerAnimationEvent,PlayerMoveEvent
+- hide_methods: getHandlers,getHandlerList,getEventName
--- src/main/java/org/erocs/gadget/Gadget.java
+++ src/main/java/org/erocs/gadget/Gadget.java
@@ -1,77 +1,250 @@
 package org.erocs.gadget;
 
-import java.lang.IllegalAccessException;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
-import org.erocs.gadget.listeners.Block;
-import org.erocs.gadget.listeners.Enchantment;
-import org.erocs.gadget.listeners.Entity;
-import org.erocs.gadget.listeners.Hanging;
-import org.erocs.gadget.listeners.Inventory;
-import org.erocs.gadget.listeners.Player;
-import org.erocs.gadget.listeners.Server;
-import org.erocs.gadget.listeners.Vehicle;
-import org.erocs.gadget.listeners.Weather;
-import org.erocs.gadget.listeners.World;
-
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Gadget extends JavaPlugin {
   private static final Logger log_ = Logger.getLogger("Gadget");
+  private static Gadget plugin_ = null;
 
-  public void onEnable() {
-    PluginManager plugman = getServer().getPluginManager();
-    plugman.registerEvents(new Block(), this);
-    plugman.registerEvents(new Enchantment(), this);
-    plugman.registerEvents(new Entity(), this);
-    plugman.registerEvents(new Hanging(), this);
-    plugman.registerEvents(new Inventory(), this);
-    plugman.registerEvents(new Player(), this);
-    plugman.registerEvents(new Server(), this);
-    plugman.registerEvents(new Vehicle(), this);
-    plugman.registerEvents(new Weather(), this);
-    plugman.registerEvents(new World(), this);
+  private boolean show_only_player_events_ = true;
+  private List<String> hide_methods_ = Arrays.asList(
+      "getHandlers",
+      "getHandlerList",
+      "getEventName");
+  private List<String> hide_events_ = Arrays.asList(
+      "PlayerAnimationEvent",
+      "PlayerMoveEvent");
+  private List<String> only_show_events_ = new ArrayList<String>();
+
+  static public Gadget getPlugin() {
+    return plugin_;
   }
 
-  static public void decodeEvent(Event event) {
-    StringBuilder sb = new StringBuilder();
-    Class cls = event.getClass();
-    sb.append(String.format("Event: %s\n", cls.getName()));
-    sb.append("  Inherits: ");
-    List<Class> hierarchy = new ArrayList<Class>();
-    Class child = event.getClass();
-    boolean not_first = false;
-    while(true) {
-      hierarchy.add(child);
-      child = child.getSuperclass();
-      if (child == null) {
+  static public void error(String msg) {
+    log_.severe("[Gadget] " + msg);
+  }
+
+  static public void warn(String msg) {
+    log_.warning("[Gadget] " + msg);
+  }
+
+  static public void info(String msg) {
+    log_.info("[Gadget] " + msg);
+  }
+
+  static public List<Class<?>> getSuperClasses(Class<?> cls) {
+    List<Class<?>> super_classes = new ArrayList<Class<?>>();
+    Class<?> child_cls = cls.getSuperclass();
+    while(child_cls != null) {
+      if (child_cls.equals(Object.class)) {
         break;
       }
+      super_classes.add(child_cls);
+      child_cls = child_cls.getSuperclass();
+    }
+    return super_classes;
+  }
+
+  static public List<Class<?>> getInterfaces(Class<?> cls) {
+    return Arrays.asList(cls.getInterfaces());
+  }
+
+  static public String describeObject(
+      Class cls,
+      List<Class<?>> super_classes,
+      List<Class<?>> interfaces,
+      String indent) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(String.format("Name: %s\n", cls.getName()));
+    StringBuilder sbch = new StringBuilder();
+    boolean not_first = false;
+    for (Class<?> child_cls : super_classes) {
       if (not_first) {
-        sb.append(", ");
+        sbch.append(", ");
       } else {
         not_first = true;
       }
-      sb.append(child.getName());
+      sbch.append(child_cls.getName());
     }
-    sb.append("\n");
+    if (sbch.length() > 0) {
+      sb.append(String.format("%s   Inherits: %s\n", indent, sbch.toString()));
+      sbch = new StringBuilder();
+    }
+    not_first = false;
+    for (Class infa : interfaces) {
+      if (not_first) {
+        sbch.append(", ");
+      } else {
+        not_first = true;
+      }
+      sbch.append(infa.getName());
+    }
+    if (sbch.length() > 0) {
+      sb.append(String.format("%s   Implements: %s\n", indent, sbch.toString()));
+    }
+    return sb.toString();
+  }
 
-//    for (Class c : hierarchy) {
-//      for (Field field : c.getDeclaredFields()) {
-//        try {
-//          sb.append(String.format("  Field %s: %s\n",
-//              field.getName(),
-//              field.get(event).getClass().getName()));
-//        } catch(IllegalAccessException e) {
-//        }
-//      }
-//    }
+  static public void decodeEvent(Event event) {
+    getPlugin().oDecodeEvent(event);
+  }
 
-    log_.info("[Gadget] " + sb.toString());
+  public void oDecodeEvent(Event event) {
+    boolean provides_player = false;
+    Class<?> cls = event.getClass();
+    String event_name = cls.getName();
+    List<Class<?>> super_classes = getSuperClasses(cls);
+    List<Class<?>> interfaces = getInterfaces(cls);
+
+    StringBuilder sb = new StringBuilder();
+    sb.append("Event ");
+    sb.append(describeObject(cls, super_classes, interfaces, ""));
+
+    // super_cls doesn't include cls
+    super_classes.add(cls);
+    for (Class<?> super_cls : super_classes) {
+      for (Method m : super_cls.getDeclaredMethods()) {
+        String method_name = m.getName();
+        if (isHiddenMethod(method_name)) {
+          continue;
+        }
+        Class[] parameter_types = m.getParameterTypes();
+        if (parameter_types.length > 0) {
+          continue;
+        }
+        try {
+          Object retval = m.invoke(event);
+          Class<?> retval_cls = retval.getClass();
+          String retval_str = "";
+          // TODO: Decode arrays
+          if (retval instanceof String) {
+            retval_str = (String)retval;
+          } else if (retval instanceof Player) {
+            retval_str = String.format("%s", ((Player)retval).getDisplayName());
+            provides_player = true;
+          } else {
+            Method to_string_meth = retval_cls.getMethod("toString");
+            if (to_string_meth != null) {
+              retval_str = (String)to_string_meth.invoke(retval);
+            }
+          }
+          // Newline included in describeObject output
+          sb.append(String.format(" Method %s='%s' type %s",
+              m.getName(),
+              retval_str,
+              describeObject(
+                  retval_cls,
+                  getSuperClasses(retval_cls),
+                  getInterfaces(retval_cls),
+                  " ")));
+        } catch (Exception e) {
+        }
+      }
+    }
+    String display = sb.toString();
+    if (shouldShowEvent(event_name) &&
+        (!onlyShowPlayerEvents() || provides_player)) {
+      info(sb.toString());
+    }
+  }
+
+  public void onEnable() {
+    registerEvents();
+    loadConfiguration();
+    plugin_ = this;
+  }
+
+  private void registerEvents() {
+    PluginManager plugman = getServer().getPluginManager();
+    plugman.registerEvents(new org.erocs.gadget.listeners.Block(), this);
+    plugman.registerEvents(new org.erocs.gadget.listeners.Enchantment(), this);
+    plugman.registerEvents(new org.erocs.gadget.listeners.Entity(), this);
+    plugman.registerEvents(new org.erocs.gadget.listeners.Hanging(), this);
+    plugman.registerEvents(new org.erocs.gadget.listeners.Inventory(), this);
+    plugman.registerEvents(new org.erocs.gadget.listeners.Player(), this);
+    plugman.registerEvents(new org.erocs.gadget.listeners.Server(), this);
+    plugman.registerEvents(new org.erocs.gadget.listeners.Vehicle(), this);
+    plugman.registerEvents(new org.erocs.gadget.listeners.Weather(), this);
+    plugman.registerEvents(new org.erocs.gadget.listeners.World(), this);
+  }
+
+  private void loadConfiguration() {
+    reloadConfig();
+    FileConfiguration config = getConfig();
+    config.options().copyDefaults(true);
+    show_only_player_events_ = config.getBoolean(
+        "show_only_player_events", show_only_player_events_);
+    hide_methods_ = getStringList(config, "hide_methods", hide_methods_);
+    hide_events_ = getStringList(config, "hide_events", hide_events_);
+    only_show_events_ = getStringList(config, "only_show_events", only_show_events_);
+  }
+
+  private boolean onlyShowPlayerEvents() {
+    return show_only_player_events_;
+  }
+
+  private boolean isHiddenMethod(String method_name) {
+    for (String hidden_method : hide_methods_) {
+      if (hidden_method.equals(method_name)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean isHiddenEvent(String event_name) {
+    for (String hidden_event : hide_events_) {
+      if (hidden_event.equals(event_name)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean isOnlyShowEvent(String event_name) {
+    for (String hidden_event : only_show_events_) {
+      if (hidden_event.equals(event_name)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean shouldShowEvent(String event_name) {
+    int last_dot = event_name.lastIndexOf('.');
+    if (last_dot >= 0) {
+      event_name = event_name.substring(last_dot + 1);
+    }
+    if (isHiddenEvent(event_name)) {
+      return false;
+    }
+    if (!only_show_events_.isEmpty()) {
+      return isOnlyShowEvent(event_name);
+    }
+    return true;
+  }
+
+  private List<String> getStringList(
+          ConfigurationSection config,
+          String path,
+          List<String> defaults) {
+    List<String> retval = config.getStringList(path);
+    if (retval == null || retval.isEmpty()) {
+      return defaults;
+    }
+    return retval;
   }
 }
